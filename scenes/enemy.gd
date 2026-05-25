deeb6ae3dd7701e54f8d8e6e452f982696ca2bce
Infornographie <140265612+Infornographie@users.noreply.github.com>
2026-05-25 16:14:37 +0200

    Enemies self collisions
    - knockback between enemies colliding for cooler swarm-like effect

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 var direction : Vector2
 var speed : float = 75
 var damage : float
+var knockback : Vector2
 var elite : bool = false:
 	set(value):
 		elite = value
@@ -24,11 +25,17 @@ func _physics_process(delta):
 	var separation = (player_reference.position - position).length() #check separation from player
 	if separation >= 1000 and not elite: #if separation is more than 500 free them from memory, except elite
 		queue_free()
-	
+
+	#Enemy will be moving towards player position
 	direction = (player_reference.position - position).normalized()
 	velocity = direction * speed
-	move_and_collide(velocity * delta)
-	#Enemy will be moving towards player position
+	knockback = knockback.move_toward(Vector2.ZERO, 1) #knockback will be decaying over time
+	velocity += knockback #adding knockback to velocity
+	
+	var collider = move_and_collide(velocity * delta)
+	if collider:
+		collider.get_collider().knockback = (collider.get_collider().global_position - global_position).normalized() * 70 #applies knockback to bodies colliding with the enemy
+
 	
 	#Turn the sprite facing
 	if direction.x > 0:
