ce3ba35d8770feeb4ffea7544252c52680290633
Infornographie <140265612+Infornographie@users.noreply.github.com>
2026-05-26 19:11:35 +0200

    Funny magician
    - overhauled thrown magician for infinite fun

--- scenes/projectile.gd
+++ scenes/projectile.gd
@@ -4,8 +4,15 @@ var direction : Vector2 = Vector2.RIGHT
 var speed : float = 200
 var damage : float = 1
 
+var time : float = 0.0
+
 func _physics_process(delta):
-	position += direction * speed * delta
+	#position += direction * speed * delta
+	#rotation += 5.0 * delta
+	time += delta
+	var wobble = direction.rotated(PI/2) * (sin(time * 7.0) * 50.0 + sin(time * 13.0) * 25.0)
+	position += (direction * speed + wobble) * delta
+	rotation += 5.0 * delta
 
 
 func _on_body_entered(body: Node2D) -> void:
